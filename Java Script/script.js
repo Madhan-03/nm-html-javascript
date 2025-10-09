bb2c5c9a72a6e801463242066b96b8674bcc4cef
Madhan-03 <madhankumar8874@gmail.com>
2025-10-09 20:45:07 +0530

    HTML & Javascript

---
+++ Java Script/script.js
@@ -0,0 +1,113 @@
+a = "Madhan"
+s1 = "TOC"
+s2 = "OS"
+s3 = "DW"
+m1 = 100
+m2 = 67
+m3 = 50
+b = "Jesvanth"
+s1 = "TOC"
+s2 = "OS"
+s3 = "DW"
+m4 = 100
+m5 = 99
+m6 = 51
+c = "Daniyal"
+s1 = "TOC"
+s2 = "OS"
+s3 = "DW"
+m7 = 100
+m8 = 45
+m9 = 86
+console.log("Student Name :",a)
+console.log("Subjects :",s1,s2,s3)
+console.log("Total Marks :",m1+m2+m3)
+console.log("Student Name :",b)
+console.log("Subjects :",s1,s2,s3)
+console.log("Total Marks :",m4+m5+m6)
+console.log("Student Name :",c)
+console.log("Subjects :",s1,s2,s3)
+console.log("Total Marks :",m7+m8+m9)
+n1 = "Madhan"
+n2 = "Bruck"
+n3 = "Jesvanth"
+s1 = "TOC"
+s2 = "OS"
+s3 = "DW"
+m1 = 100,m2 = 56,m3 = 95
+tot1 = m1+m2+m3
+avg1 = m1+m2+m3/3
+m4 = 100,m5 = 99,m6 = 51
+tot2 = m4+m5+m6
+avg2 = m4+m5+m6/3
+m7 = 100,m8 = 99,m9 = 65
+tot3 = m7+m8+m9
+avg3 = m7+m8+m9/3
+console.log("Student Name :",n1)
+console.log("Department :","CSE")
+console.log("Subjects :",s1,s2,s3)
+console.log("Total Marks :",tot1)
+console.log("Student's Average",avg1)
+console.log("Student Name :",n2)
+console.log("Department :","EEE")
+console.log("Subjects :",s1,s2,s3)
+console.log("Total Marks :",tot2)
+console.log("Student's Average",avg2)
+console.log("Student Name :",n3)
+console.log("Department :","ECE")
+console.log("Subjects :",s1,s2,s3)
+console.log("Total Marks :",tot3)
+console.log("Student's Average",avg3)
+x = 100
+y = --x
+y = ++x
+y = --x
+console.log(--y)
+console.log(--x)
+let score = 45
+if(score >= 45)
+{
+    console.log("You Pass !")
+}
+else
+{
+    console.log("You Fail !")
+}
+day = 5
+switch(day)
+{
+    case 1:
+        console.log("Monday")
+        break
+    case 2:
+        console.log("Tuesday")
+        break 
+    case 3:
+        console.log("Wednesday")
+        break 
+    case 4:
+        console.log("Thursday")
+        break
+    case 5:
+        console.log("Friday")
+        break
+    case 6:
+        console.log("Saturday")
+        break
+    case 7:
+        console.log("Sunday")
+        break
+    default:
+        console.log("Invalid Day")
+        break
+}
+list = ["Madhan","Bavitran","Baren Bruck","Alwin","Daniyal"]
+console.log(list)
+list.push("Anand")
+console.log(list)
+list.pop()
+console.log(list)
+list.shift("Madhan")
+console.log(list)
+list.unshift("Madhan")
+console.log(list)
